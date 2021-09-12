7d2d43e6f109300a917d73c165ee103efd04dbbb
Sergei S <div@altertech.com>
2021-09-12 22:46:54 +0200

    examples

--- examples/client.rs
+++ examples/client.rs
@@ -4,7 +4,7 @@ use serde_json::Value;
 fn main() {
     let mut db = YedbClient::new("tcp://127.0.0.1:8870");
     let key_name = "test/key1";
-    db.key_set(&key_name, Value::from(123u8)).unwrap();
+    db.key_set(&key_name, Value::from(123_u8)).unwrap();
     println!("{:?}", db.key_get(&key_name));
     db.key_delete(&key_name).unwrap();
 }
--- examples/embed.rs
+++ examples/embed.rs
@@ -6,7 +6,7 @@ fn main() {
     db.set_db_path(&"/tmp/db1").unwrap();
     db.open().unwrap();
     let key_name = "test/key1";
-    db.key_set(&key_name, Value::from(123u8)).unwrap();
+    db.key_set(&key_name, Value::from(123_u8)).unwrap();
     println!("{:?}", db.key_get(&key_name));
     db.key_delete(&key_name).unwrap();
     db.close().unwrap();
